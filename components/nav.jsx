fa3be27e2024fade5d03eb36285e7856f1170665
dimentio233 <johnnyhe7@outlook.com>
2023-06-22 15:52:52 -0700

    added nav pages, style.css

--- components/nav.jsx
+++ components/nav.jsx
@@ -2,6 +2,14 @@ import React from "react"
 
 export default function nav() {
     return (
-        <h1>Nav goes here</h1>
+        <nav className="nav-container">
+            <a className="nav-tag" href="">Home</a>
+            <a className="nav-tag" href="">Advertise</a>
+            <a className="nav-tag" href="">Distribute</a>
+            <div className="nav-logo">Brew Banner</div>
+            <a className="nav-tag" href="">Contact</a>
+            <a className="nav-tag" href="">Support</a>
+            <a className="nav-tag" href="">Socials</a>
+        </nav>
     )
 }
