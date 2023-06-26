de1c7dae3c94327922ce6bba1c6961b49b227ab7
dimentio233 <johnnyhe7@outlook.com>
2023-06-25 18:16:59 -0700

    made "landingtwo" component responsive, except buttons

--- components/body.jsx
+++ components/body.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Landing from "./Landing"
+import LandingTwo from "./LandingTwo"
 
 export default function body() {
     return (
         <>
-        <Landing/>
+        {/* <Landing/> */}
+        <LandingTwo />
         </>
     )
 }
