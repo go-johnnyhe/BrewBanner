8334caa754420e2989b96a69ac0dec2ccec54143
kyleung1 <leungkyle@gmail.com>
2023-06-28 15:34:15 -0700

    advertise

---
+++ components/LearnAdvertise.jsx
@@ -0,0 +1,29 @@
+const LearnAdvertise = () => {
+    return (
+        <>
+            <h1 style={{textAlign: 'center', paddingLeft: '38%', paddingRight: '38%'}}>Brew Up Your Brand On Our Unique Platform</h1>
+            <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+               <div className="LearnAdvertiseBox">
+                    <p className="bigNum">30</p>
+                    <p className="mediumText">minutes</p>
+                    <p className="smallText">of extended personal connection</p>
+                </div>
+                <div className="LearnAdvertiseBox">
+                    <p className="bigNum">10</p>
+                    <p className="mediumText">impressions</p>
+                    <p className="smallText">for each potential customer</p>
+                </div>
+                <div className="LearnAdvertiseBox">
+                    <p className="bigNum">20</p>
+                    <p className="mediumText">distinct exposures</p>
+                    <p className="smallText">as a mobile billboard</p>
+                </div>
+            </div>
+            <div style={{display: 'flex', 'justifyContent': 'center', margin: '30px'}}>
+                <button>Learn More</button>
+            </div>
+        </>
+    )
+}
+
+export default LearnAdvertise
--- components/body.jsx
+++ components/body.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Landing from "./Landing"
+import LearnAdvertise from "./LearnAdvertise"
 
 export default function body() {
     return (
         <>
         <Landing/>
+        <LearnAdvertise/>
         </>
     )
 }
