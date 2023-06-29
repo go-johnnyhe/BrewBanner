5ba70b624b01cdb0cd0d43cc9771cad8be831f04
kyleung1 <leungkyle@gmail.com>
2023-06-28 17:27:04 -0700

    a

--- components/body.jsx
+++ components/body.jsx
@@ -1,17 +1,26 @@
-import React from "react"
-import Landing from "./Landing"
-import LandingTwo from "./LandingTwo"
-import LearnAdvertise from "./LearnAdvertise"
-import Locations from "./Locations"
-import Mission from "./Mission"
-import Promote from "./Promote"
 
+export default function body() {
+    return (
+        <>
+        <LandingTwo/>
+        <LearnAdvertise/>
+        <Locations/>
+        <Mission/>
+        <div style={{display: 'flex', 'justifyContent': 'center'}}>\
+                <a href="/contact">
+                        <button>Contact Us</button>        
+                </a>
+        </div>
+        </>
+    )
+}
 export default function body() {
     return (
         <>
         {/* <Landing/> */}
         <LandingTwo />
         <LearnAdvertise/>
+        <Promote/>
         </>
     )
 }
