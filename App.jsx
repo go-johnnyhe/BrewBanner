7fdd2917410f4005a468d2c1c11e97c485bb805f
dimentio233 <johnnyhe7@outlook.com>
2023-07-05 23:10:29 -0700

    jsx is back

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 // import Nav from "./components/Nav"
-import Body from "./components/Body"
+import Body from "./components/Body.jsx"
 
 export default function App() {
     return (
