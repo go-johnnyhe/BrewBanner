16d55d33f1f3a7240a11c6b8adf6c94fe24627e0
dimentio233 <johnnyhe7@outlook.com>
2023-07-05 21:11:37 -0700

    fixed indentation

--- components/nav.jsx
+++ components/nav.jsx
@@ -2,31 +2,31 @@ import React from "react"
 
 export default function Nav() {
 
-    // const [responsive, setResponsive] = React.useState(false);
+  // const [responsive, setResponsive] = React.useState(false);
 
-    // function toggleResponsive(){
-    //     setResponsive(prev => !prev);
-    // } 
+  // function toggleResponsive(){
+  //     setResponsive(prev => !prev);
+  // } 
 
 
-    return (
-    
-                    <section className="top-nav">
-                    <div className="nav-logo">
-                      BrewBanner
-                    </div>
-                    <input id="menu-toggle" type="checkbox" />
-                    <label className='menu-button-container' htmlFor="menu-toggle">
-                    <div className='menu-button'></div>
-                  </label>
-                    <ul className="menu">
-                      <li>Home</li>
-                      <li>Advertise</li>
-                      <li>Distribute</li>
-                      <li>Contact</li>
-                      <li>Support</li>
-                      <li>Socials</li>
-                    </ul>
-                  </section>
-    )
+  return (
+
+    <section className="top-nav">
+      <div className="nav-logo">
+        BrewBanner
+      </div>
+      <input id="menu-toggle" type="checkbox" />
+      <label className='menu-button-container' htmlFor="menu-toggle">
+        <div className='menu-button'></div>
+      </label>
+      <ul className="menu">
+        <li>Home</li>
+        <li>Advertise</li>
+        <li>Distribute</li>
+        <li>Contact</li>
+        <li>Support</li>
+        <li>Socials</li>
+      </ul>
+    </section>
+  )
 }
