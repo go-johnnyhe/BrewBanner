2fd1490fc3a91126ee6b442e5f65dde1c6c07b34
dimentio233 <johnnyhe7@outlook.com>
2023-07-05 22:30:02 -0700

    pushing again!

--- components/LandingTwo.jsx
+++ components/LandingTwo.jsx
@@ -8,7 +8,7 @@ export default function LandingTwo() {
             <img className="landing-img" src="../assets/Brew Banner2.png"/>
             <img className="landing-img" src="../assets/Brew Banner3.png"/>
             <img className="landing-img" src="../assets/Brew Banner4.png"/>
-            d
+            d1
             <img className="landing-img" src="../assets/Brew Banner5.png"/>
             <img className="landing-img" src="../assets/Brew Banner6.png"/>
         </div> */}
