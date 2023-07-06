62805aeaa8fdd6a65f8f2936e505be7eba9858b5
dimentio233 <johnnyhe7@outlook.com>
2023-07-05 21:51:13 -0700

    added public and images folder

--- components/LandingTwo.jsx
+++ components/LandingTwo.jsx
@@ -12,12 +12,12 @@ export default function LandingTwo() {
             <img className="landing-img" src="../assets/Brew Banner6.png"/>
         </div> */}
         <div className="landing-img-container">
-            <img className="landing-img-1" src="../assets/Brew Banner1.png"/>
-            <img className="landing-img-2" src="../assets/Brew Banner2.png"/>
-            <img className="landing-img-3" src="../assets/Brew Banner3.png"/>
-            <img className="landing-img-4" src="../assets/Brew Banner4.png"/>
-            <img className="landing-img-5" src="../assets/Brew Banner5.png"/>
-            <img className="landing-img-6" src="../assets/Brew Banner6.png"/>
+            <img className="landing-img-1" src="../assets/images/Brew Banner1.png"/>
+            <img className="landing-img-2" src="../assets/images/Brew Banner2.png"/>
+            <img className="landing-img-3" src="../assets/images/Brew Banner3.png"/>
+            <img className="landing-img-4" src="../assets/images/Brew Banner4.png"/>
+            <img className="landing-img-5" src="../assets/images/Brew Banner5.png"/>
+            <img className="landing-img-6" src="../assets/images/Brew Banner6.png"/>
         </div>
         <div className="landing-buttons">
         <button>Advertise with us</button>
