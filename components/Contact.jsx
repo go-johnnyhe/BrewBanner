ec15a07e3080488c9a22d1698bccc1b5b46643e6
dimentio233 <johnnyhe7@outlook.com>
2023-07-10 17:54:21 -0700

    Pushing the contact info email setup

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,19 +1,101 @@
+import emailjs from 'emailjs-com'
+import { Form, Input, TextArea, Button } from 'semantic-ui-react'
+import Swal from 'sweetalert2'
+
+
+const SERVICE_ID = "service_885cw97";
+const TEMPLATE_ID = "template_76uilkx";
+const USER_ID = "aA9YXj15SJU7mu_rf";
+
+
+
 const Contact = () => {
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+          .then((result) => {
+            console.log(result.text);
+            Swal.fire({
+              icon: 'success',
+              title: 'Message Sent Successfully'
+            })
+          }, (error) => {
+            console.log(error.text);
+            Swal.fire({
+              icon: 'error',
+              title: 'Ooops, something went wrong',
+              text: error.text,
+            })
+          });
+        e.target.reset()
+    }
+
     return (
-        <>
-            <h1 style={{textAlign: 'center', paddingLeft: '38%', paddingRight: '38%'}}>Contact Us</h1>
-            <form>
-                <div style={{display: 'flex', 'justifyContent': 'center'}}>
-                    <input type="email" id='contact-email' placeholder="Email" style={{width: '50%', maxWidth: '800px'}}/><br/>
-                </div>
-                <div style={{display: 'flex', 'alignItems': 'center', 'flexDirection': 'column'}}>
-                    <textarea type="text" id='contact-text' style={{height: '200px', width: '50%', maxWidth: '800px', margin: '20px'}}/><br/>
-                    <button value="Submit">Submit</button>
-                </div>
-                
-            </form>
-        </>
+    <div className="App">
+      <Form onSubmit={handleOnSubmit}>
+        <Form.Field
+          id='form-input-control-email'
+          control={Input}
+          label='Email'
+          name='user_email'
+          placeholder='Email…'
+          required
+          icon='mail'
+          iconPosition='left'
+        />
+        <Form.Field
+          id='form-input-control-last-name'
+          control={Input}
+          label='Name'
+          name='user_name'
+          placeholder='Name…'
+          required
+          icon='user circle'
+          iconPosition='left'
+        />
+        <Form.Field
+          id='form-textarea-control-opinion'
+          control={TextArea}
+          label='Message'
+          name='user_message'
+          placeholder='Message…'
+          required
+        />
+        <Button type='submit' color='green'>Submit</Button>
+      </Form>
+    </div>
     )
 }
 
 export default Contact
+
+// import React, { useRef } from 'react';
+// import emailjs from '@emailjs/browser';
+
+// export default function Contact() {
+//   const form = useRef();
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs.sendForm('service_885cw97', 'template_76uilkx', form.current, 'aA9YXj15SJU7mu_rf')
+//       .then((result) => {
+//           console.log(result.text);
+//       }, (error) => {
+//           console.log(error.text);
+//       });
+//   };
+
+//   return (
+//     <form ref={form} onSubmit={sendEmail}>
+//       <label>Name</label>
+//       <input type="text" name="user_name" />
+//       <label>Email</label>
+//       <input type="email" name="user_email" />
+//       <label>Message</label>
+//       <textarea name="message" />
+//       <input type="submit" value="Send" />
+//     </form>
+//   );
+// };
