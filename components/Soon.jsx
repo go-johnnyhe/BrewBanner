eba02a99531eaa50b8a126f68cbde0fffc6628a1
dimentio233 <johnnyhe7@outlook.com>
2023-07-13 13:50:01 -0700

    added Soon.jsx as "Coming Soon" page

---
+++ components/Soon.jsx
@@ -0,0 +1,9 @@
+export default function Soon() {
+  return (
+    <div className="soon-container">
+      <div className="soon-text">
+        <h1>Coming Soon</h1>
+      </div>
+    </div>
+  );
+}
