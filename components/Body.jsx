614deff7c9f7cf76cc5950ab991f101ed029f915
dimentio233 <johnnyhe7@outlook.com>
2023-07-13 14:19:47 -0700

    pulling button change

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,19 +1,20 @@
-import React from "react"
-import LandingTwo from "./LandingTwo"
-import LearnAdvertise from "./LearnAdvertise"
-import Locations from "./Locations"
-import Promote from "./Promote"
-import Footer from "./Footer"
-
+import React from "react";
+import LandingTwo from "./LandingTwo";
+import LearnAdvertise from "./LearnAdvertise";
+import Locations from "./Locations";
+import Promote from "./Promote";
+import Footer from "./Footer";
+import Soon from "./Soon";
 
 export default function Body() {
-    return (
-        <div className="main-page">
-        <LandingTwo/>
-        <LearnAdvertise/>
-        <Promote />
-        <Locations />
-        <Footer />
-        </div>
-    )
+  return (
+    // <div className="main-page">
+    // <LandingTwo/>
+    // <LearnAdvertise/>
+    // <Promote />
+    // <Locations />
+    // <Footer />
+    // </div>
+    <Soon />
+  );
 }
--- components/Soon.jsx
+++ components/Soon.jsx
@@ -2,7 +2,7 @@ export default function Soon() {
   return (
     <div className="soon-container">
       <div className="soon-text">
-        <h1>Coming Soon</h1>
+        <h1>Under construction</h1>
       </div>
     </div>
   );
